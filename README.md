2b99528377c39a96b11b3ee0fc0fed72aece5267
Abrarhusamfahim <fahimjahin321@gmail.com>
2021-10-19 21:30:38 +0600

    edited some codes

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-Abrar Husam Fahim
+## Abrar Husam Fahim
 * Professional English Course
 
 * Description: I tried my best and this is my first professional English courses website and the website created entirely by me.
 
 Live Site Link: https://frosty-fermat-71658a.netlify.app/
 
-Technology we use to build this project:
+## Technology we use to build this project:
 
 * JSX
 * NODE.js
 * React.js
-* Bootstrap
+* React Bootstrap
 * React Router
 * Fire Base
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './NotFound.css'
-import nonfound from '../../images/sua-loi-404-tren-may-tinh.1PNG-1.png'
+import not from '../../images/not.png'
+import { useHistory } from 'react-router';
 const NotFound = () => {
+    const history = useHistory()
+    const homeFoundHandler = ()=>{
+        history.push('/home')
+    }
+    const backFoundHandler = ()=>{
+        history.push('/services')
+    }
     return (
-        <div className="notfound">
-            <img src={nonfound} alt="" />
+        <div className="overflow-hidden notFound">
+            <img src={not} alt="" />
+            <button onClick={homeFoundHandler} className="me-5 button1">Home Page</button>
+            <button onClick={backFoundHandler} className="button2">Back</button>
         </div>
     );
 };
